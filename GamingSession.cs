9de2141b55f1a4cddf3b30dfd37d8087df02f2f4
Victor <vovovkaba@gmail.com>
2023-11-07 23:48:59 -0800

    Added the GamingSession class

---
+++ GamingSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingTracker
+{
+    internal class GamingSession
+    {
+        public int Id;
+        public DateTime StartTime;
+        public DateTime EndTime;
+        public int Duration;
+    }
+}
