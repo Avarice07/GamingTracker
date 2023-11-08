7d504d20232d2edcbfecab83f804104865ed3933
Victor <vovovkaba@gmail.com>
2023-11-08 01:39:47 -0800

    Added a config file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace GamingTracker
+﻿
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace GamingTracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var name = ConfigurationManager.AppSettings["Key0"];
+            DateTime date1 = new DateTime(2023, 11, 7, 22, 30 ,50); 
+            Console.WriteLine(DateTime.Now.Subtract(date1)+" " +name);
         }
     }
 }
