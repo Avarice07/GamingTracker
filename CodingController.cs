26e080bc9e9d5478ba9b9ada2fd23e17a002c8c9
Victor <vovovkaba@gmail.com>
2023-11-09 19:38:25 -0800

    Added database functions

--- CodingController.cs
+++ CodingController.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace GamingTracker
 {
     internal static class CodingController
     {
+
         public static void CreateTable() 
         {
-            using (var connection = new SqliteConnection("Data Source=game-Session.db"))
+            using (var connection = new SqliteConnection("ConfigurationManager.AppSettings[\"Key0\"]"))
             {
                 connection.Open();
 
@@ -28,6 +31,84 @@ namespace GamingTracker
 
             }
         }
-        
+
+        public static void Insert()
+        {
+            using (var connection = new SqliteConnection("ConfigurationManager.AppSettings[\"Key0\"]"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"INSERT INTO game_session (Id, Start, End, Duration)
+                  VALUES (@param1, @param2, @param3, @param4)
+                     )";
+
+                command.Parameters.AddWithValue("@param1", Name);
+                command.Parameters.AddWithValue("@param2", Name);
+                command.Parameters.AddWithValue("@param3", Name);
+                command.Parameters.AddWithValue("@param4", Name);
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+
+        public static void Update()
+        {
+            using (var connection = new SqliteConnection("ConfigurationManager.AppSettings[\"Key0\"]"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"UPDATE gaming_session SET Start=@param2, End=@param3, Duration=@param4 WHERE Id=@param1)";
+
+                command.Parameters.AddWithValue("@param1", Name);
+                command.Parameters.AddWithValue("@param2", Name);
+                command.Parameters.AddWithValue("@param3", Name);
+                command.Parameters.AddWithValue("@param4", Name);
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+
+        public static void Delete()
+        {
+            using (var connection = new SqliteConnection("ConfigurationManager.AppSettings[\"Key0\"]"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"DELETE FROM game_session
+                  WHERE Id=@param1";
+
+                command.Parameters.AddWithValue("@param1", Name);
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+
+        public static void View()
+        {
+            using (var connection = new SqliteConnection("ConfigurationManager.AppSettings[\"Key0\"]"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"INSERT INTO game_session (Id, Start, End, Duration)
+                  VALUES (@param1, @param2, @param3, @param4)
+                     )";
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+        }
+
+
     }
 }
